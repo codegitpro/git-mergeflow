86ade56b7dffb451f642f88de0cb74f212925481
codegitpro <michael.jin.dev@gmail.com>
2020-12-11 00:12:10 +0400

    feat(test-rebase-commit): test rebase commit

--- file-rebase.js
+++ file-rebase.js
@@ -1,3 +1,3 @@
 // this is the test file about checking git conflict 
 // if user make same file on two branch at same time
-// and rebase them, not make merge commit
+// I have made file 5 and change it
---
+++ file5.js
@@ -0,0 +1,2 @@
+const file5= 5
+const totalFile = file5
